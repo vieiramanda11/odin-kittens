ac7f8a9115ba6cf413ccc1fa817467ae1cbacf7d
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-01 13:40:30 -0300

    initializing repository and creating model, controller ans index view

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,48 @@
+class KittensController < ApplicationController
+  
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.udpate(kitten_params)
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_url
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'kittens#index'
+  resources :kittens
+end
